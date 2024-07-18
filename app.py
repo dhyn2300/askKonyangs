7f6f428be41210f426ae09455357238803b9ed99
oyonm23 <darvy2106@gmail.com>
2024-07-18 18:54:53 +0900

    Modify app.py with new functionality

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ import os
 import time
 from openai import OpenAI, Configuration
 
-app = Flask(__name__)
 
-client = OpenAI(api_key='sk-proj-wtIQXSPfoLhe8ZSwHxhiT3BlbkFJjzpIBsC5ibTq7TuWlOAu')
-
-assistant_id = "asst_aUkQNBqjwNpAKs0k39oZLGUd"
-thread_id = ""
 
 # OpenAI 클라이언트 설정
 
